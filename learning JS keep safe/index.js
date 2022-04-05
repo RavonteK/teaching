5029f82c2daf927f72473913774ca52d7c9feae2
EyobY0 <102843810+EyobY0@users.noreply.github.com>
2022-04-05 18:11:59 -0500

    Add files via upload
    there

---
+++ learning JS keep safe/index.js
@@ -0,0 +1,31 @@
+console.log("ravonte likes men");
+console.log("be safe");
+
+window.alert("ravonte likes men");
+
+//this is comment
+/* 
+   this 
+   is
+   a 
+   multiline 
+   comment
+*/
+
+// two steps:
+// 1.declaration (var, let , const)
+// 2. assignment (= assignment oprator)
+
+
+let ravonte = "gay"; //strings
+let rg = 0; //numbers 
+let student = false //boleans 
+
+rg = rg + 1;
+
+console.log("ravonte is",ravonte);// ravonte = gay  
+console.log(rg) // rg = the number 0
+console.log("straght", student); // student = false 
+
+document. getElementById("p1").innerHTML = "hello " + gay;
+document. getElementById("p2").innerHTML = "how many girls " + rg  
